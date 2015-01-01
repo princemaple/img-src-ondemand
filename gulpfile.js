6e9091bdc25801e13c9843f5551a6f32f64e25c3
Po Chen <pchen@zendesk.com>
2015-01-01 17:58:39 +1100

    Add ngAnnotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 
 var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
 
-gulp.task('concat', function() {
+gulp.task('compile', function() {
   return gulp.src('src/**/*.js')
+    .pipe(ngAnnotate())
     .pipe(concat('img-src-ondemand.js'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['concat']);
+  gulp.watch('src/**/*.js', ['compile']);
 });
 
-gulp.task('default', ['concat']);
+gulp.task('default', ['compile']);
